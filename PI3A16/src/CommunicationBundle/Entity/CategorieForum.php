53f16aa2b0a430fa2a2fae00a2fa5b5715639e97
aminlaz <47175686+aminlaz@users.noreply.github.com>
2019-02-17 14:45:54 +0100

    entity communication w event

---
+++ PI3A16/src/CommunicationBundle/Entity/CategorieForum.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace CommunicationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CategorieForum
+ *
+ * @ORM\Table(name="categorie_forum")
+ * @ORM\Entity(repositoryClass="CommunicationBundle\Repository\CategorieForumRepository")
+ */
+class CategorieForum
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return CategorieForum
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
+
