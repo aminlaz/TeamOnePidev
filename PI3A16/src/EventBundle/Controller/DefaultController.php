1265d503651fe7858cc591129506981c8bca810a
RamiZaxs <47175660+RamiZaxs@users.noreply.github.com>
2019-02-17 12:40:28 +0100

    bundles

--- PI3A16/src/EventBundle/Controller/DefaultController.php
+++ PI3A16/src/EventBundle/Controller/DefaultController.php
@@ -6,10 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('@Event/Default/index.html.twig');
-    }
+
     public function eventAction()
     {
         return $this->render('@Event/Default/event.html.twig');
--- PI3A16/src/ProfilingBundle/Controller/DefaultController.php
+++ PI3A16/src/ProfilingBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ProfilingBundle:Default:index.html.twig');
+        return $this->render('@Profiling/Default/index.html.twig');
     }
 }
