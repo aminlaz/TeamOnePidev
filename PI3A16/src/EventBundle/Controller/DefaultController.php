ac5a531e4796ad0441b3967f4434d5e7d9bbce42
RamiZaxs <47175660+RamiZaxs@users.noreply.github.com>
2019-02-18 18:30:22 +0100

    addevent

--- PI3A16/src/EventBundle/Controller/DefaultController.php
+++ PI3A16/src/EventBundle/Controller/DefaultController.php
@@ -11,5 +11,9 @@ class DefaultController extends Controller
     {
         return $this->render('@Event/Default/event.html.twig');
     }
+    public function addeventAction()
+    {
+        return $this->render('@Event/Default/addevent.html.twig');
+    }
 
 }
