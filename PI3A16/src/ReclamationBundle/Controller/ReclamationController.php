b12e86839574a1828dfd717d98b9d60b6bca1814
AtefMaddouri <47184233+AtefMaddouri@users.noreply.github.com>
2019-02-20 14:23:55 +0100

    ,

---
+++ PI3A16/src/ReclamationBundle/Controller/ReclamationController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace ReclamationBundle\Controller;
+
+use ReclamationBundle\Entity\Reclamation;
+use ReclamationBundle\Form\ReclamationType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Reclamation controller.
+ *
+ */
+class ReclamationController extends Controller
+{
+    /**
+     * Lists all reclamation entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reclamations = $em->getRepository('ReclamationBundle:Reclamation')->findAll();
+
+        return $this->render('@Reclamation/Reclamation/index.html.twig', array(
+            'reclamations' => $reclamations,
+        ));
+    }
+
+    /**
+     * Creates a new reclamation entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+
+        $reclamation = new Reclamation();
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $reclamation->setDateR(new \DateTime);
+            $reclamation->setTraite(false);
+            $reclamation->setCorbeille(false);
+            $reclamation->setArchive(false);
+
+            $em->persist($reclamation);
+            $em->flush();
+
+            return $this->redirectToRoute('reclamation_show', array('id' => $reclamation->getId()));
+        }
+
+        return $this->render('@Reclamation/Reclamation/new.html.twig', array(
+            'reclamation' => $reclamation,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a reclamation entity.
+     *
+     */
+    public function showAction(Reclamation $reclamation)
+    {
+        $deleteForm = $this->createDeleteForm($reclamation);
+
+        return $this->render('@Reclamation/Reclamation/show.html.twig', array(
+            'reclamation' => $reclamation,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing reclamation entity.
+     *
+     */
+    public function editAction(Request $request, Reclamation $reclamation)
+    {
+        $deleteForm = $this->createDeleteForm($reclamation);
+        $editForm = $this->createForm('ReclamationBundle\Form\ReclamationType', $reclamation);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('reclamation_edit', array('id' => $reclamation->getId()));
+        }
+
+        return $this->render('@Reclamation/Reclamation/edit.html.twig', array(
+            'reclamation' => $reclamation,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a reclamation entity.
+     *
+     */
+    public function deleteAction(Request $request, Reclamation $reclamation)
+    {
+        $form = $this->createDeleteForm($reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($reclamation);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('reclamation_index');
+    }
+
+    /**
+     * Creates a form to delete a reclamation entity.
+     *
+     * @param Reclamation $reclamation The reclamation entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Reclamation $reclamation)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('reclamation_delete', array('id' => $reclamation->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
